5bcb2398d7ddb16846175a4bb2ee7beef8eb60a0
esarr13 <emmanuelbenoitsarr@gmail.com>
2022-05-17 23:19:49 -0400

    navigation bar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 import './Navbar.scss'
+import { images } from '../../constants';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(true);
+    const menus = ['home', 'about', 'work', 'skills', 'contact']
     return (
-        <div>Navbar</div>
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                {/* <img src={images.logo} alt="logo" /> */}
+                <h1>Emmanuel SARR</h1>
+            </div>
+            <ul className='app__navbar-links'>
+                {menus.map((menu) => {
+                    return (
+                        <li key={`link-${menu}`} className='app__flex p-text'>
+                            <div />
+                            <a href={`#${menu}`}>{menu}</a>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <div className='app__navbar-menu'>
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+
+                {toggle && (
+                    <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: 'easeOut' }}>
+                        <ul >
+                            <HiX onClick={() => setToggle(false)} />
+                            {menus.map((menu) => {
+                                return (
+                                    <li key={menu}>
+                                        <a href={`#${menu}`} onClick={() => setToggle(false)}>{menu}</a>
+                                    </li>
+                                )
+                            })}
+
+                        </ul>
+                    </motion.div>)
+                }
+            </div>
+        </nav>
     )
 }
 
